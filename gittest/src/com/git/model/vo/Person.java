54da8a8cf913ecda404a1fc19dbb2e02f374b728
LEE <LEE@DESKTOP-H41RDQH>
2019-08-29 21:09:42 +0900

    add person

--- gittest/src/com/git/model/vo/Person.java
+++ gittest/src/com/git/model/vo/Person.java
@@ -2,18 +2,7 @@ package com.git.model.vo;
 
 public class Person {
 	
-	private String name;
-	private String var;
-
-	public Person(String name, String var) {
-		super();
-		this.name = name;
-		this.var = var;
-	}
-
-	public Person() {
-		// TODO Auto-generated constructor stub
-	}
+	
 	
 	
 }
