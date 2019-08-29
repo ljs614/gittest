5d059b2c8e41989a039ef07d8e6280fb31d5c9e5
LEE <LEE@DESKTOP-H41RDQH>
2019-08-29 21:03:04 +0900

    commit

--- gittest/src/com/git/model/vo/Person.java
+++ gittest/src/com/git/model/vo/Person.java
@@ -1,5 +1,20 @@
 package com.git.model.vo;
 
 public class Person {
+	
+	private String name;
+	private String var;
 
+	public Person(String name, String var) {
+		super();
+		this.name = name;
+		this.var = var;
+	}
+
+	public Person() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
 }
+
